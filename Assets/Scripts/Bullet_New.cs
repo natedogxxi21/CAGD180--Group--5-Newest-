4b197e23aebb2a8b5bc8cf7b234f20e1fbc20959
Nathan <nathan.marquez0021@gmail.com>
2025-04-21 18:21:39 -0700

    Enemy Interaction Complete
    FINALYY Bullets properly kill enemies. Had to make a new prefab for enemies. Old prefab did not work for whatever reason.
    -Added Particle effect to show Enemies explode

--- Assets/Scripts/Bullet_New.cs
+++ Assets/Scripts/Bullet_New.cs
@@ -6,6 +6,7 @@ public class Bullet_New : MonoBehaviour
 {
 
     public float speed = 20f;
+    public int damage = 1; //Bullet does 1 damage
     public Rigidbody rb;
 
     // Start is called before the first frame update
@@ -14,10 +15,17 @@ public class Bullet_New : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
-    void OnTriggerEnter(Collider hitInfo)
+    void OnTriggerEnter(Collider collision)
     {
+        Enemy enemy = collision.GetComponent<Enemy>();
+        Debug.Log("Bullet hit something: ");
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
 
         Destroy(gameObject);
 
     }
 }
+
